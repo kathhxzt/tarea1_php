edb4c966e6ace17e1d70cad3769464d7c14ec217
kathhxzt <19980466@clases.edu.sv>
2026-05-18 17:14:53 -0600

    Repositorio
    este archivo contiene los enlaces hacia cada uno de los ejercicios realizados, permitiendo acceder facilmente a cada practica

---
+++ index.php
@@ -0,0 +1,157 @@
+<?php
+
+echo "<h1> Problema 1 </h1>";
+echo '<a href = "p1.html">';
+echo "siguiente";
+echo "</a>";
+echo "<br><br>";
+
+
+echo "<h1> Problema 2 </h1>";
+echo '<a href = "p2.html">';
+echo "siguiente";
+echo "</a>";
+echo "<br><br>";
+
+echo "<h1> Problema 3 </h1>";
+echo '<a href = "p3.html">';
+echo "siguiente";
+echo "</a>";
+echo "<br><br>";
+
+echo "<h1> Problema 4 </h1>";
+echo '<a href = "p4.html">';
+echo "siguiente";
+echo "</a>";
+echo "<br><br>";
+
+echo "<h1> Problema 5 </h1>";
+echo '<a href = "p5.html">';
+echo "siguiente";
+echo "</a>";
+echo "<br><br>";
+
+echo "<h1> Problema 6 </h1>";
+echo '<a href = "p6.html">';
+echo "siguiente";
+echo "</a>";
+echo "<br><br>";
+
+echo "<h1> Problema 7 </h1>";
+echo '<a href = "p7.html">';
+echo "siguiente";
+echo "</a>";
+echo "<br><br>";
+
+
+echo "<h1> Problema 8 </h1>";
+echo '<a href = "p8.html">';
+echo "siguiente";
+echo "</a>";
+echo "<br><br>";
+
+echo "<h1> Problema 9 </h1>";
+echo '<a href = "p9.html">';
+echo "siguiente";
+echo "</a>";
+echo "<br><br>";
+
+echo "<h1> Problema 10 </h1>";
+echo '<a href = "p10.html">';
+echo "siguiente";
+echo "</a>";
+echo "<br><br>";
+
+echo "<h1> Problema 11 </h1>";
+echo '<a href = "p11.html">';
+echo "siguiente";
+echo "</a>";
+echo "<br><br>";
+
+echo "<h1> Problema 12 </h1>";
+echo '<a href = "p12.html">';
+echo "siguiente";
+echo "</a>";
+echo "<br><br>";
+
+
+echo "<h1> Problema 13 </h1>";
+echo '<a href = "p13.html">';
+echo "siguiente";
+echo "</a>";
+echo "<br><br>";
+
+echo "<h1> Problema 14 </h1>";
+echo '<a href = "p14.html">';
+echo "siguiente";
+echo "</a>";
+echo "<br><br>";
+
+echo "<h1> Problema 15 </h1>";
+echo '<a href = "p15.html">';
+echo "siguiente";
+echo "</a>";
+echo "<br><br>";
+
+echo "<h1> Problema 16 </h1>";
+echo '<a href = "p16.html">';
+echo "siguiente";
+echo "</a>";
+echo "<br><br>";
+
+echo "<h1> Problema 17 </h1>";
+echo '<a href = "p17.html">';
+echo "siguiente";
+echo "</a>";
+echo "<br><br>";
+
+
+echo "<h1> Problema 18 </h1>";
+echo '<a href = "p18.html">';
+echo "siguiente";
+echo "</a>";
+echo "<br><br>";
+
+echo "<h1> Problema 19 </h1>";
+echo '<a href = "p19.html">';
+echo "siguiente";
+echo "</a>";
+echo "<br><br>";
+
+echo "<h1> Problema 20 </h1>";
+echo '<a href = "p20.html">';
+echo "siguiente";
+echo "</a>";
+echo "<br><br>";
+
+echo "<h1> Problema 21 </h1>";
+echo '<a href = "p21.html">';
+echo "siguiente";
+echo "</a>";
+echo "<br><br>";
+
+echo "<h1> Problema 22 </h1>";
+echo '<a href = "p22.html">';
+echo "siguiente";
+echo "</a>";
+echo "<br><br>";
+
+echo "<h1> Problema 23 </h1>";
+echo '<a href = "p23.html">';
+echo "siguiente";
+echo "</a>";
+echo "<br><br>";
+
+echo "<h1> Problema 24 </h1>";
+echo '<a href = "p24.html">';
+echo "siguiente";
+echo "</a>";
+echo "<br><br>";
+
+echo "<h1> Problema 25 </h1>";
+echo '<a href = "p25.html">';
+echo "siguiente";
+echo "</a>";
+echo "<br><br>";
+
+?>
